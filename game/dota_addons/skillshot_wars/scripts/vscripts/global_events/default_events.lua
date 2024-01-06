8479957ba17bb3d27b649f28ce5adfe26fd5eec2
Maksim Sukhodolskii <maksukh@inbox.ru>
2024-01-06 07:24:11 +0300

    Remove nonexistent item_tpscroll and another crash

--- game/dota_addons/skillshot_wars/scripts/vscripts/global_events/default_events.lua
+++ game/dota_addons/skillshot_wars/scripts/vscripts/global_events/default_events.lua
@@ -323,7 +323,6 @@ end
 function GameMode:OnPlayerChat(keys)
   local teamonly = keys.teamonly
   local userID = keys.userid
-  local playerID = self.vUserIds[userID]:GetPlayerID()
 
   local text = keys.text
 end
